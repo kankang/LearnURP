b676b095f41eac756f318ba6a5f5e339dc357d04
kankang <kankang@66y.com>
2022-03-23 20:18:18 +0800

    1.5 转换空间

---
+++ CustomRP/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,8 @@
+﻿#ifndef CUSTOM_UNITY_INPUT_INCLUDE
+#define CUSTOM_UNITY_INPUT_INCLUDE
+
+float4x4 unity_ObjectToWorld;
+
+float4x4 unity_MatrixVP;
+
+#endif
--- CustomRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
+++ CustomRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
@@ -1,9 +1,12 @@
 ﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDE
 #define CUSTOM_UNLIT_PASS_INCLUDE
 
-float4 UnlitPassVertex() : SV_POSITION
+#include "../ShaderLibrary/Common.hlsl"
+
+float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION
 {
-	return 0.0;
+	float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+	return TransformWorldToHClip(positionWS);
 }
 
 float4 UnlitPassFragment() : SV_TARGET
