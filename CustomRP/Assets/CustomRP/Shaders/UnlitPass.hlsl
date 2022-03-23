857b299b59f899087e63ceba3dfc61d7c893d43d
kankang <kankang@66y.com>
2022-03-23 20:00:15 +0800

    1.4 Shader 函数

--- CustomRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
+++ CustomRP/Assets/CustomRP/Shaders/UnlitPass.hlsl
@@ -1,4 +1,14 @@
 ﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDE
 #define CUSTOM_UNLIT_PASS_INCLUDE
 
+float4 UnlitPassVertex() : SV_POSITION
+{
+	return 0.0;
+}
+
+float4 UnlitPassFragment() : SV_TARGET
+{
+	return 0.0;
+}
+
 #endif
