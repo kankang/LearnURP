69a4f3fe464c763a8687345be0a662b4ee87b13d
kankang <kankang@66y.com>
2022-03-23 20:38:34 +0800

    1.6 核心库

--- CustomRP/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
+++ CustomRP/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -2,7 +2,11 @@
 #define CUSTOM_UNITY_INPUT_INCLUDE
 
 float4x4 unity_ObjectToWorld;
-
+float4x4 unity_WorldToObject;
+float4x4 unity_MatrixV;
 float4x4 unity_MatrixVP;
+float4x4 glstate_matrix_projection;
+
+real4 unity_WorldTransformParams;
 
 #endif
