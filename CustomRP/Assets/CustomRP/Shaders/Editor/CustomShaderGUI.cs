25e0523178a000d2816cdaddd0d828c478ce32fa
kankang <kankang@66y.com>
2022-03-28 17:02:44 +0800

    5.2 设置属性和关键字

--- CustomRP/Assets/CustomRP/Shaders/Editor/CustomShaderGUI.cs
+++ CustomRP/Assets/CustomRP/Shaders/Editor/CustomShaderGUI.cs
@@ -6,8 +6,81 @@ using UnityEngine.Rendering;
 
 public class CustomShaderGUI : ShaderGUI
 {
+    MaterialEditor editor;
+    Object[] materials;
+    MaterialProperty[] properties;
+
+    bool Clipping
+    {
+        set => SetProperty("_Clipping", "_CLIPPING", value);
+    }
+
+    bool PremultiplyAlpha
+    {
+        set => SetProperty("_PremulAlpha", "_PREMULTIPLY_ALPHA", value);
+    }
+
+    BlendMode SrcBlend
+    {
+        set => SetProperty("_SrcBlend", (float)value);
+    }
+
+    BlendMode DstBlend
+    {
+        set => SetProperty("_DstBlend", (float)value);
+    }
+
+    RenderQueue RenderQueue
+    {
+        set
+        {
+            foreach(Material m in materials)
+            {
+                m.renderQueue = (int)value;
+            }
+        }
+    }
+
+    bool ZWrite
+    {
+        set => SetProperty("_ZWrite", value ? 1f : 0f);
+    }
+
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
         base.OnGUI(materialEditor, properties);
+
+        editor = materialEditor;
+        materials = materialEditor.targets;
+        this.properties = properties;
+    }
+
+    void SetProperty(string name, float value)
+    {
+        FindProperty(name, properties).floatValue = value;
+    }
+
+    void SetProperty(string name, string keyword, bool value)
+    {
+        SetProperty(name, value ? 1f : 0f);
+        SetKeyword(keyword, value);
+    }
+
+    void SetKeyword(string keyword, bool enabled)
+    {
+        if (enabled)
+        {
+            foreach (Material m in materials)
+            {
+                m.EnableKeyword(keyword);
+            }
+        }
+        else
+        {
+            foreach (Material m in materials)
+            {
+                m.EnableKeyword(keyword);
+            }
+        }
     }
 }
