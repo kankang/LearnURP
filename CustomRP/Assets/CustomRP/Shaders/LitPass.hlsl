320d652d61cc86a8e0413e33747cb8fdfd74e33a
kankang <kankang@66y.com>
2022-03-30 16:49:10 +0800

    2.5 灯光衰减

--- CustomRP/Assets/CustomRP/Shaders/LitPass.hlsl
+++ CustomRP/Assets/CustomRP/Shaders/LitPass.hlsl
@@ -19,16 +19,14 @@ UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
 	UNITY_DEFINE_INSTANCED_PROP(float, _Smoothness)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
-struct Attributes
-{
+struct Attributes {
 	float3 positionOS : POSITION;
 	float3 normalOS : NORMAl;
 	float2 baseUV : TEXCOORD0;
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
-struct Varyings
-{
+struct Varyings {
 	float4 positionCS : SV_POSITION;
 	float3 positionWS : VAR_POSITION;
 	float3 normalWS : VAR_NORMAL;
@@ -36,27 +34,7 @@ struct Varyings
 	UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
-float SampleDirectionalShadowAtlas(float3 positionSTS)
-{
-	return SAMPLE_TEXTURE2D_SHADOW(_DirectionalShadowAtlas, SHADOW_SAMPLER, positionSTS);
-}
-
-float GetDirectionalShadowAttenuation(DirectionalShadowData data, Surface surfaceWS)
-{
-	if (data.strength <= 0.0)
-		return 1.0;
-
-	float3 positionSTS = mul(_DirectionalShadowMatrices[data.tileIndex],
-		float4(surfaceWS.position, 1.0)
-		).xyz;
-
-	float shadow = SampleDirectionalShadowAtlas(positionSTS);
-
-	return lerp(1.0, shadow, data.strength);
-}
-
-Varyings LitPassVertex(Attributes input)// : SV_POSITION
-{
+Varyings LitPassVertex(Attributes input) {
 	Varyings output;
 	UNITY_SETUP_INSTANCE_ID(input);
 	UNITY_TRANSFER_INSTANCE_ID(input, output);
@@ -69,8 +47,7 @@ Varyings LitPassVertex(Attributes input)// : SV_POSITION
 	return output;
 }
 
-float4 LitPassFragment(Varyings input) : SV_TARGET
-{
+float4 LitPassFragment(Varyings input) : SV_TARGET {
 	UNITY_SETUP_INSTANCE_ID(input);
 	float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
 	float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
