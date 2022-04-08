9e54160ce9bd2992372f356e8e547eedaa1ec935
kankang <kankang@66y.com>
2022-04-08 17:26:55 +0800

    5.2 裁切阴影

--- CustomRP/Assets/CustomRP/Shaders/ShadowCasterPass.hlsl
+++ CustomRP/Assets/CustomRP/Shaders/ShadowCasterPass.hlsl
@@ -54,7 +54,7 @@ void ShadowCasterPassFragment(Varyings input)
 	float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
 	float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
 	float4 base = baseMap * baseColor;
-	#if defined(_CLIPPING)
+	#if defined(_SHADOWS_CLIP)
 		clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
 	#endif
 }
