208ce49d1a74501cd8fbbe9c05eddcef9d3fdf07
kankang <kankang@66y.com>
2022-04-08 17:28:46 +0800

    5.3 抖动阴影

--- CustomRP/Assets/CustomRP/Shaders/ShadowCasterPass.hlsl
+++ CustomRP/Assets/CustomRP/Shaders/ShadowCasterPass.hlsl
@@ -56,6 +56,9 @@ void ShadowCasterPassFragment(Varyings input)
 	float4 base = baseMap * baseColor;
 	#if defined(_SHADOWS_CLIP)
 		clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+	#elif defined(_SHADOWS_DITHER)
+		float dither = InterleavedGradientNoise(input.positionCS.xy, 0);
+		clip(base.a - dither);
 	#endif
 }
 
